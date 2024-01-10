3f4d01a6bcfb0725abd2e4c785f77fe2c5b4e2d1
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-10 11:17:54 +0500

    modification : contact us form, delte 3d animation about , improve broekn links

--- frontend/scripts/contact-us.js
+++ frontend/scripts/contact-us.js
@@ -14,49 +14,60 @@ document
       document.getElementById("contact-error-msg").innerText = "";
       document.getElementById("contact-submit").innerText = "Sending...";
 
-      const response = await axios
-        .post("http://localhost:8000/api/feedback/addFeedback", {
-          userName,
-          Email,
-          Phone,
-          Message,
-        })
-        .then((response) => {
-          if (response.status === 201) {
-            document.getElementById("contact-submit").innerText = "Submit";
+      try {
+        const response = await axios
+          .post("http://localhost:8000/api/feedback/addFeedback", {
+            userName,
+            Email,
+            Phone,
+            Message,
+          })
+          .then((response) => {
+            if (response.status === 201) {
+              document.getElementById("contact-submit").innerText = "Submit";
 
-            document.getElementById("contact-name").value = "";
-            document.getElementById("contact-email").value = "";
-            document.getElementById("contact-phone").value = "";
-            document.getElementById("contact-message").value = "";
+              document.getElementById("contact-name").value = "";
+              document.getElementById("contact-email").value = "";
+              document.getElementById("contact-phone").value = "";
+              document.getElementById("contact-message").value = "";
 
-            document
-              .getElementById("contact-error-msg")
-              .classList.remove("text-red-500");
-            document
-              .getElementById("contact-error-msg")
-              .classList.add("text-green-500");
-            document.getElementById("contact-error-msg").innerText =
-              "Message sent!";
-
-            setTimeout(() => {
               document
                 .getElementById("contact-error-msg")
-                .classList.remove("text-green-500");
+                .classList.remove("text-red-500");
               document
                 .getElementById("contact-error-msg")
-                .classList.add("text-red-500");
-              document.getElementById("contact-error-msg").innerText = "";
-            }, 3000);
-          } else {
-            document.getElementById("contact-error-msg").innerText =
-              "Something went wrong!";
+                .classList.add("text-green-500");
+              document.getElementById("contact-error-msg").innerText =
+                "Message sent!";
 
-            setTimeout(() => {
-              document.getElementById("contact-error-msg").innerText = "";
-            }, 3000);
-          }
-        });
+              setTimeout(() => {
+                document
+                  .getElementById("contact-error-msg")
+                  .classList.remove("text-green-500");
+                document
+                  .getElementById("contact-error-msg")
+                  .classList.add("text-red-500");
+                document.getElementById("contact-error-msg").innerText = "";
+              }, 3000);
+            } else {
+              document.getElementById("contact-submit").innerText = "Submit";
+              document.getElementById("contact-error-msg").innerText =
+                "Something went wrong!";
+
+              setTimeout(() => {
+                document.getElementById("contact-error-msg").innerText = "";
+              }, 3000);
+            }
+          });
+      } catch (err) {
+        document.getElementById("contact-submit").innerText = "Submit";
+        document.getElementById("contact-error-msg").innerText =
+          "Something went wrong!";
+
+        setTimeout(() => {
+          document.getElementById("contact-error-msg").innerText = "";
+        }, 3000);
+      }
     } else {
       document.getElementById("contact-error-msg").innerText =
         "Please fill all fields!";
