de7ed435cf0b3b14cb95ca32050fd2810faf54ea
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-10 10:39:52 +0500

    Implemented available cars on home page

---
+++ frontend/scripts/cars.js
@@ -0,0 +1,36 @@
+// cars
+
+const getAllCars = async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost:8000/api/car/getAllCars"
+    );
+    const carsCardsInfo = response.data.data;
+    let cardsHTML = "";
+
+    carsCardsInfo.forEach((card) => {
+      cardsHTML += `
+              <div class="rounded overflow-hidden shadow-lg">
+              <img class="w-full h-[200px] overflow-hidden object-cover" src=${card.ImageUrl} alt=${card.Title}>
+              <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">${card.Title}</div>
+              <p class="text-gray-700 text-base">
+                  ${card.Description}
+              </p>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#${card.Tag}</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+          </div>
+              `;
+    });
+
+    document.querySelector(".cars-container").innerHTML = cardsHTML;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.addEventListener("load", getAllCars);
